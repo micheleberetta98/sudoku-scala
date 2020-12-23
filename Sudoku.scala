37fe745cd3f4ebd68280341dd1c9a047d6f63e38
Michele Beretta <beretta.m98@gmail.com>
2020-12-23 19:07:06 +0100

    added sudoku solving capabilities

--- Sudoku.scala
+++ Sudoku.scala
@@ -1,35 +1,52 @@
 class Sudoku(var sudoku: Array[Array[Int]]) {
-  def isPossible(x: Int, y: Int, n: Int) = {
-    !(isInRow(x, n) || isInCol(y, n) || isInSquare(x, y, n))
+  def solve(): Boolean = {
+    for (i <- 0 until 9)
+      for (j <- 0 until 9)
+        if (sudoku(i)(j) == 0)
+          return tryAllNumbers(i, j)
+    return true
   }
 
-  private def isInRow(x: Int, n: Int) = {
+  def tryAllNumbers(i: Int, j: Int): Boolean = {
+    for (n <- 1 until 10)
+      if (isPossible(i, j, n)) {
+        sudoku(i)(j) = n
+        if (solve())
+          return true
+        sudoku(i)(j) = 0
+      }
+    return false
+  }
+
+  private def isPossible(x: Int, y: Int, n: Int) =
+    !(isInRow(x, n) || isInCol(y, n) || isInSquare(x, y, n))
+
+  private def isInRow(x: Int, n: Int): Boolean = {
     for (j <- 0 until 9)
       if (sudoku(x)(j) == n)
-        true
+        return true
     false
   }
 
-  private def isInCol(y: Int, n: Int) = {
+  private def isInCol(y: Int, n: Int): Boolean = {
     for (i <- 0 until 9)
       if (sudoku(i)(y) == n)
-        true
+        return true
     false
   }
 
-  private def isInSquare(x: Int, y: Int, n: Int) = {
-    val x0 = x / 3
-    val y0 = y / 3
+  private def isInSquare(x: Int, y: Int, n: Int): Boolean = {
+    val x0 = (x / 3) * 3
+    val y0 = (y / 3) * 3
     for (i <- 0 until 3)
       for (j <- 0 until 3)
         if (sudoku(x0 + i)(y0 + j) == n)
-          true
+          return true
     false
   }
 
-  override def toString(): String = {
+  override def toString(): String =
     sudoku.map(rowToString).mkString("\n")
-  }
 
   private def rowToString(row: Array[Int]) =
     row.map(x => x.toString).mkString(" ")
--- SudokuHandler.scala
+++ SudokuHandler.scala
@@ -1,9 +1,6 @@
 import java.io.PrintWriter
 
 class SudokuHandler extends Sudoku(Array.ofDim(9, 9)) with FileIO {
-  def solve() = 
-    println("TODO: Implement")
-
   def readFromFile(filename: String) = {
     val lines = io.Source.fromFile(filename).mkString.split("\n")
     var row = 0
