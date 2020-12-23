fdb955c540160d0e68e9d6f94b9f73c314b67429
Michele Beretta <beretta.m98@gmail.com>
2020-12-23 22:07:03 +0100

    added cli arguments support

--- Main.scala
+++ Main.scala
@@ -1,13 +1,33 @@
-object Main extends App {
-  val sudoku = new SudokuHandler()
+object Main {
+  def main(args: Array[String]) = {
+    if (validateArgs(args)) {
+      val input = args(0)
+      val output = if (args.length > 1) Some(args(1)) else None
+      solveSudoku(input, output)
+    } else {
+      println("USAGE:")
+      println("  scala Main INPUT_FILE [OUTPUT_FILE]")
+    }
+  }
 
-  sudoku.readFromFile("test-data/input.txt")
-  val solved = sudoku.solve()
-  if (solved) {
-    println("The sudoku was solved!")
-    println(sudoku.toPrettyString())
-    sudoku.writeToFile("test-data/output.txt")
-  } else {
-    println("The sudoku is unsolvable")
+  def validateArgs(args: Array[String]) = {
+    args.length > 0
+  }
+
+  def solveSudoku(input: String, output: Option[String]) = {
+    val sudoku = new SudokuHandler()
+
+    sudoku.readFromFile(input)
+    val solved = sudoku.solve()
+    if (solved) {
+      println("The sudoku was solved!")
+      println(sudoku.toPrettyString())
+      output match {
+        case Some(file) => sudoku.writeToFile(file)
+        case None       => ()
+      }
+    } else {
+      println("The sudoku is unsolvable")
+    }
   }
 }
