7be08e6554fca36622a2de7da3163fc9f7acbb4b
Michele Beretta <beretta.m98@gmail.com>
2020-12-23 16:39:46 +0100

    renamed Sudoku to SudokuSolver

--- Main.scala
+++ Main.scala
@@ -1,5 +1,5 @@
 object Main extends App {
-  val sudoku = new Sudoku()
+  val sudoku = new SudokuSolver()
 
   sudoku.readFromFile("test-data/input.txt")
   sudoku.writeToFile("test-data/output.txt")
